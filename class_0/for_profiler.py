c0f5335d997c1da8503d730751992a321e072683
Joao P Dubas <joao.dubas@gmail.com>
2011-11-27 20:51:25 -0200

    improve table results representation

--- class_0/for_profiler.py
+++ class_0/for_profiler.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 from __future__ import division
 import sys
 from timeit import Timer
@@ -21,15 +22,15 @@ def for_listcomp():
     [10 * i for i in run_range]
 
 def create_table_result(title, results):
-    head = 'min mean max'.split(' ')
+    head = 'fn min mean max'.split(' ')
     calc = {
+        'fn': title,
         'min': min(results),
         'mean': sum(results) / len(results),
         'max': max(results)
     }
-    sys.stdout.write('\nResultados para: %s' % title)
-    sys.stdout.write('\n{0:<10s} {1:<10s} {2:<10s}'.format(*head))
-    sys.stdout.write('\n{min:<10.5f} {mean:<10.5f} {max:<10.5f}\n'.format(**calc))
+    sys.stdout.write('{0:>30s} {1:<10s} {2:<10s} {3:<10s}'.format(*head))
+    sys.stdout.write('\n{fn:>30s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(**calc))
 
 def profile(stmt='pass', setup='pass'):
     t = Timer(stmt, setup)
@@ -38,7 +39,7 @@ def profile(stmt='pass', setup='pass'):
     create_table_result(stmt, r)
 
 if __name__ == '__main__':
-    sys.stdout.write('Processando uma lista de 1000 elementos. Repetindo 10 vezes, 1000 execuções.\n')
+    # sys.stdout.write(u'Processando uma lista de 1000 elementos. Repetindo 10 vezes, 1000 execuções.\n')
     fn_list = 'for_traditional for_traditional_with_lambda for_listcomp for_listcomp_with_lambda'.split(' ')
     for fn in fn_list:
         profile('{}()'.format(fn), 'from __main__ import {}'.format(fn))
