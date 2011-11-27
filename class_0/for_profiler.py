bae7db08890d63801c5aa2f26968f543c7cc5aae
Joao P Dubas <joao.dubas@gmail.com>
2011-11-27 19:44:23 -0200

    using timeit.Timer to profile

--- class_0/for_profiler.py
+++ class_0/for_profiler.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-multiply_by_10 = lambda x: 10 * x
+# -*- encoding: utf-8 -*-
+from __future__ import division
+from timeit import Timer
 
+multiply_by_10 = lambda x: 10 * x
 run_range = range(0, 1000)
 
 def for_traditional():
@@ -9,3 +12,24 @@ def for_traditional():
 
 def for_listcomp():
     [multiply_by_10(i) for i in run_range]
+
+def create_table_result(title, results):
+    head = 'min mean max'.split(' ')
+    calc = {
+        'min': min(results),
+        'mean': sum(results) / len(results),
+        'max': max(results)
+    }
+    print '\nResultados para: %s' % title
+    print '{0:10} {1:10} {2:10}'.format(*head)
+    print '{min:10.5f} {mean:10.5f} {max:10.5f}'.format(**calc)
+
+def profile(stmt='pass', setup='pass'):
+    t = Timer(stmt, setup)
+    r = t.repeat(10, 1000)
+
+    create_table_result(stmt, r)
+
+print 'Processando uma lista de 1000 elementos. Repetindo 10 vezes, 1000 execuções.\n'
+profile('for_traditional()', 'from __main__ import for_traditional')
+profile('for_listcomp()', 'from __main__ import for_listcomp')
