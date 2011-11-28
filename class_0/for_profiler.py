2bd654defcd622165678427e0ba6e547f0a5cceb
Joao P Dubas <joao.dubas@gmail.com>
2011-11-27 22:33:25 -0200

    fix traditional method to populate a list and add new tests

--- class_0/for_profiler.py
+++ class_0/for_profiler.py
@@ -5,21 +5,42 @@ import sys
 from timeit import Timer
 
 multiply_by_10 = lambda x: 10 * x
-run_range = range(0, 1000)
+min_range = 0
+max_range = 1000
 
 def for_traditional_with_lambda():
+    run_range = range(min_range, max_range)
+    mul_range = []
     for i in run_range:
-        multiply_by_10(i)
-
-def for_listcomp_with_lambda():
-    [multiply_by_10(i) for i in run_range]
+        mul_range.append(multiply_by_10(i))
+    return mul_range
 
 def for_traditional():
+    run_range = range(min_range, max_range)
+    mul_range = []
     for i in run_range:
-        10 * i
+        mul_range.append(10 * i)
+    return mul_range
+
+def for_traditional_with_lambda_same_list():
+    run_range = range(min_range, max_range)
+    for i, v in enumerate(run_range):
+        run_range[i] = multiply_by_10(v)
+    return run_range
+
+def for_traditional_same_list():
+    run_range = range(min_range, max_range)
+    for i, v in enumerate(run_range):
+        run_range[i] = v * 10
+    return run_range
+
+def for_listcomp_with_lambda():
+    run_range = range(min_range, max_range)
+    return [multiply_by_10(i) for i in run_range]
 
 def for_listcomp():
-    [10 * i for i in run_range]
+    run_range = range(min_range, max_range)
+    return [10 * i for i in run_range]
 
 def create_table_result(title, results):
     head = 'fn min mean max'.split(' ')
@@ -29,8 +50,8 @@ def create_table_result(title, results):
         'mean': sum(results) / len(results),
         'max': max(results)
     }
-    sys.stdout.write('{0:>30s} {1:<10s} {2:<10s} {3:<10s}'.format(*head))
-    sys.stdout.write('\n{fn:>30s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(**calc))
+    sys.stdout.write('{0:>40s} {1:<10s} {2:<10s} {3:<10s}'.format(*head))
+    sys.stdout.write('\n{fn:>40s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(**calc))
 
 def profile(stmt='pass', setup='pass'):
     t = Timer(stmt, setup)
@@ -39,7 +60,6 @@ def profile(stmt='pass', setup='pass'):
     create_table_result(stmt, r)
 
 if __name__ == '__main__':
-    # sys.stdout.write(u'Processando uma lista de 1000 elementos. Repetindo 10 vezes, 1000 execuções.\n')
-    fn_list = 'for_traditional for_traditional_with_lambda for_listcomp for_listcomp_with_lambda'.split(' ')
+    fn_list = 'for_traditional for_traditional_with_lambda for_traditional_same_list for_traditional_with_lambda_same_list for_listcomp for_listcomp_with_lambda'.split(' ')
     for fn in fn_list:
         profile('{}()'.format(fn), 'from __main__ import {}'.format(fn))
