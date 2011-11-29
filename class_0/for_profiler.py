0239e77753c736e0d085cee02488aba4192d2688
Joao P Dubas <joao.dubas@gmail.com>
2011-11-28 22:22:17 -0200

    renamed test funcions

--- class_0/for_profiler.py
+++ class_0/for_profiler.py
@@ -8,41 +8,42 @@ multiply_by_10 = lambda x: 10 * x
 min_range = 0
 max_range = 1000
 
-def for_traditional_with_lambda():
+def test_for_loop_with_lambda():
     run_range = range(min_range, max_range)
     mul_range = []
     for i in run_range:
         mul_range.append(multiply_by_10(i))
     return mul_range
 
-def for_traditional():
+def test_for_loop():
     run_range = range(min_range, max_range)
     mul_range = []
     for i in run_range:
         mul_range.append(10 * i)
     return mul_range
 
-def for_traditional_with_lambda_same_list():
+def test_for_loop_with_lambda_same_list():
     run_range = range(min_range, max_range)
     for i, v in enumerate(run_range):
         run_range[i] = multiply_by_10(v)
     return run_range
 
-def for_traditional_same_list():
+def test_for_loop_same_list():
     run_range = range(min_range, max_range)
     for i, v in enumerate(run_range):
         run_range[i] = v * 10
     return run_range
 
-def for_listcomp_with_lambda():
+def test_listcomp_with_lambda():
     run_range = range(min_range, max_range)
     return [multiply_by_10(i) for i in run_range]
 
-def for_listcomp():
+def test_listcomp():
     run_range = range(min_range, max_range)
     return [10 * i for i in run_range]
 
-def create_table_result(title, results):
+def create_table_result(title, results, **kwargs):
+    fn_name_len = kwargs.pop('fn_name_len', len(title))
     head = 'fn min mean max'.split(' ')
     calc = {
         'fn': title,
@@ -50,16 +51,20 @@ def create_table_result(title, results):
         'mean': sum(results) / len(results),
         'max': max(results)
     }
-    sys.stdout.write('{0:>40s} {1:<10s} {2:<10s} {3:<10s}'.format(*head))
-    sys.stdout.write('\n{fn:>40s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(**calc))
+    sys.stdout.write('{0:>{fn_name_len}s} {1:<10s} {2:<10s} {3:<10s}'.format(fn_name_len=fn_name_len, *head))
+    sys.stdout.write('\n{fn:>{fn_name_len}s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(fn_name_len=fn_name_len, **calc))
 
-def profile(stmt='pass', setup='pass'):
+def profile(stmt='pass', setup='pass', **kwargs):
     t = Timer(stmt, setup)
     r = t.repeat(10, 1000)
 
-    create_table_result(stmt, r)
+    create_table_result(stmt, r, **kwargs)
 
 if __name__ == '__main__':
-    fn_list = 'for_traditional for_traditional_with_lambda for_traditional_same_list for_traditional_with_lambda_same_list for_listcomp for_listcomp_with_lambda'.split(' ')
+    fn_list = 'test_for_loop test_for_loop_with_lambda test_for_loop_same_list test_for_loop_with_lambda_same_list test_listcomp test_listcomp_with_lambda'.split(' ')
+    fn_len = max([len(fn) for fn in fn_list]) + 2
+    resp_list = [locals().get(fn)() for fn in fn_list]
+    assert resp_list.count(resp_list[0]) == len(resp_list)
+
     for fn in fn_list:
-        profile('{}()'.format(fn), 'from __main__ import {}'.format(fn))
+        profile('{}()'.format(fn), 'from __main__ import {}'.format(fn), fn_name_len=fn_len)
